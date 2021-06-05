6ed862657fa8fab75c32d48273e934cf87255695
Kursat Ufuk Coskun <kursat.ufuk@gmail.com>
2021-06-06 00:28:48 +0300

    Added native test.

--- src/test/java/org/agoncal/quarkus/starting/NativeBookResourceIT.java
+++ src/test/java/org/agoncal/quarkus/starting/NativeBookResourceIT.java
@@ -2,8 +2,27 @@ package org.agoncal.quarkus.starting;
 
 import io.quarkus.test.junit.NativeImageTest;
 
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
 @NativeImageTest
 public class NativeBookResourceIT extends BookResourceTest {
 
     // Execute the same tests but in native mode.
+
+    @Override
+    public void shouldGetABook() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParam("id", 1)
+                .when().get("/api/books/{id}")
+                .then()
+                .statusCode(200)
+                .body("title", is("Understanding Quarkus"))
+                .body("author", is("Antonio"))
+                .body("genre", is("Information Tech"));
+    }
 }
